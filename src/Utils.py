2752b1c61241574fd72d5cfcbe23aecce0428c2d
MarcBarreto <marcelohrnq@gmail.com>
2024-11-26 12:09:44 -0300

    feat: main.py

--- src/Utils.py
+++ src/Utils.py
@@ -1,6 +1,10 @@
+import os
+import glob
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from PIL import Image
+from tqdm import tqdm
 
 def plot(x, y = None, title = None, xlabel = None, ylabel = None, save = False, path = None):
     plt.figure()
@@ -33,3 +37,45 @@ def run_image(path, ipha, class2idx):
     ipha.compare_images(image, label, x_important, x_non_important)
 
     return x_important, x_non_important
+
+def run_images(folder_path, ipha, label_constant, class2idx, save_path='./'):
+    images = glob.glob(f'{folder_path}/*.[jp][pn]g')
+
+    results = {'image_path': [],
+            'features_important': [],
+            'features_non_important': [],
+            'original_score': [],
+            'important_score': [],
+            'non_important_score': [],
+            'fi_important': [],
+            'fi_non_important': [],
+            'pr_important': [],
+            'pr_non_important': [],
+            'constant': []}
+
+    for _, path in enumerate(tqdm(images, desc="Processing Image", unit="image", ncols=100)):
+        results['image_path'].append(path)
+        
+        image = Image.open(path)
+        
+        label = class2idx[path.split('/')[-2]]
+        
+        x_important, x_non_important = ipha(image, label)
+
+        results['features_important'].append(x_important)
+
+        results['features_non_important'].append(x_non_important)
+        
+        org_scr, impt_scr, non_impt_scr, fi_impt, fi_non_impt, pr_impt, pr_non_impt = ipha.compare_images(image, label, x_important, x_non_important, show = False)
+
+        results['original_score'].append(org_scr)
+        results['important_score'].append(impt_scr)
+        results['non_important_score'].append(non_impt_scr)
+        results['fi_important'].append(fi_impt)
+        results['fi_non_important'].append(fi_non_impt)
+        results['pr_important'].append(pr_impt)
+        results['pr_non_important'].append(pr_non_impt)
+        results['constant'].append(label_constant)
+
+    df = pd.DataFrame(results)
+    df.to_csv(os.join.path(save_path, 'results.csv'), index=False)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,25 @@
+import os
+import sys
 import Utils
-from PIL import Image
 from Resnet import ResNet
-from IPHA import IPHA_HC, IPHA_GA
+from IPHA import IPHA_GA
 from torchvision import transforms as T
 
 def main():
+    save_path = '../'
+
+    if len(sys.argv) > 4:
+        image_path = sys.argv[1]
+        model_path = sys.argv[2]
+        label_constant = sys.argv[3]
+        save_path = sys.argb[4]
+    elif len(sys.argv) == 3:
+        image_path = sys.argv[1]
+        model_path = sys.argv[2]
+        label_constant = sys.argv[3]
+    else:
+        raise ValueError("Insufficient arguments. Please provide at least the image path, model path, the constant type (black, white, gaussian, or norm), and optionally the save path.")
+
     transform = T.Compose([
         T.Resize(256),
         T.CenterCrop(224),
@@ -14,7 +29,7 @@ def main():
 
     resnet = ResNet(name = 'resnet18', pt = True, transform = transform)
 
-    resnet.load_model(f'/kaggle/input/resnet18/pytorch/default/1/{resnet.name}.pt')
+    resnet.load_model(model_path)
 
     class2idx = {'airplane': 0,
             'automobile': 1,
@@ -27,11 +42,25 @@ def main():
             'ship': 8,
             'truck': 9
         }
-    
-    image_path = '/kaggle/input/cifar10-pngs-in-folders/cifar10/test/dog/0001.png'
-    ipha = IPHA_GA(resnet, 0, 1000, 100, select = 10)
-    
-    Utils.run_images(image_path, ipha, class2idx)
-    
+
+    if label_constant == 'black':
+        constant = 0
+    elif label_constant == 'white':
+        constant = 255
+    elif label_constant in ['gaussian', 'norm']:
+        constant = None
+    else:
+        raise ValueError(f"Invalid label_constant: {label_constant}")
+
+    ipha = IPHA_GA(resnet, constant, 1000, 50, select = 10)
+
+    if os.path.isdir(image_path):
+        Utils.run_images(image_path, ipha, label_constant, class2idx, save_path)
+    elif os.path.isfile(image_path):
+        Utils.run_image(image_path, ipha, class2idx)
+    else:
+        raise ValueError(f"The specified image path '{image_path}' is neither a valid file nor a directory.")
+
+
 if __name__ == '__main__':
     main()

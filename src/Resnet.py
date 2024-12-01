1dfd0293a16d9bfbc2c57cd5715cda65720c2563
MarcBarreto <marcelohrnq@gmail.com>
2024-12-01 17:50:07 -0300

    feat: get_noise; get_cifar10_testset

--- src/Resnet.py
+++ src/Resnet.py
@@ -1,6 +1,8 @@
 import torch
+import numpy as np
 import torch.nn.functional as F
 from torch import nn
+from scipy.special import softmax
 
 def conv3x3(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
@@ -33,7 +35,6 @@ class BasicBlock(nn.Module):
         out = F.relu(out)
         
         return out
-
 
 class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
@@ -75,3 +76,27 @@ class ResNet(nn.Module):
         device = 'cuda' if torch.cuda.is_available() else 'cpu'
         tm = torch.load(path, map_location=device)        
         self.load_state_dict(tm)
+
+def get_resnet_cifar10():
+    print("Loading... ResNet")    
+    torch_model = ResNet(BasicBlock, [3,4,6,3], num_classes=10)
+    torch_model.load()
+    if torch.cuda.is_available():
+        torch_model.cuda() 
+    torch_model.params = list(torch_model.parameters())
+    torch_model.eval()
+    return torch_model
+
+def infer(model, x, target):
+    if isinstance(x, np.ndarray):
+        if torch.cuda.is_available():
+            x = torch.from_numpy(x.astype(np.float32)).cuda()
+        else:
+            x = torch.from_numpy(x.astype(np.float32))
+    model.eval()
+    if torch.cuda.is_available():
+        preds = softmax(model(x.cuda()).detach().cpu().numpy(), 1)
+    else:
+        preds = softmax(model(x).detach().cpu().numpy(), 1)
+    pred = preds[:,target].flatten()
+    return pred
--- src/Utils.py
+++ src/Utils.py
@@ -1,10 +1,96 @@
 import os
 import glob
+import torch
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import torch.nn.functional as F
 from PIL import Image
 from tqdm import tqdm
+from torchvision import datasets, transforms
+
+def reproducibility():
+    torch.manual_seed(50)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(50)
+
+def cpu(ob):
+    for i in range(len(ob)):
+        for j in range(len(ob[i])):
+            ob[i][j] = ob[i][j].cpu()
+    return ob
+
+def cuda(ob):
+    for i in range(len(ob)):
+        for j in range(len(ob[i])):
+            ob[i][j] = ob[i][j].cuda()
+    return ob
+
+def batch_sublogits(torch_model, batch, target):
+    batch_logits = torch_model(batch)
+    batch_probs = F.softmax(batch_logits)
+    batch_features = torch_model.features.detach().data
+    list_features = torch.split(batch_features, 1)
+    
+    list_sublogits = [(torch_model.linear.weight*feature)[target,:].cpu().detach().data.numpy() for feature in list_features]
+    
+    list_sublogits = [feature.astype(np.float32) for feature in list_sublogits] 
+    batch_logits = batch_logits.cpu().detach().data.numpy().astype(np.float32)
+    batch_probs = batch_probs.cpu().detach().data.numpy().astype(np.float32)
+
+    return batch_logits, batch_probs, list_sublogits
+
+def batch_layer(torch_model, batch):
+    batch_logits = torch_model(batch)
+    batch_probs = F.softmax(batch_logits)
+    batch_features = torch_model.features.detach().data
+    fmaps1 = torch_model.out1.cpu().detach().data
+    fmaps2 = torch_model.out2.cpu().detach().data
+    fmaps3 = torch_model.out3.cpu().detach().data
+
+    return fmaps1, fmaps2, fmaps3
+
+def get_noise(noise_type):
+    img_noise = None
+    
+    if noise_type == "black":
+        img_noise = np.zeros((3, 32, 32), dtype=np.float32)
+    elif noise_type == "white":
+        img_noise = np.ones((3, 32, 32), dtype=np.float32)
+    elif noise_type == "gaussian":
+        img_noise = np.ones((3, 32, 32), dtype=np.float32)
+        for i in range(32):
+            for j in range(32):
+                img_noise[0, i, j] = np.random.normal(loc=0.4914, scale=0.2471, size=1)
+                img_noise[1, i, j] = np.random.normal(loc=0.4822, scale=0.2435, size=1)
+                img_noise[2, i, j] = np.random.normal(loc=0.4465, scale=0.2616, size=1)
+    elif noise_type == "norm_mean":
+        img_noise = np.concatenate([
+            np.ones((1, 32, 32), dtype=np.float32) * 0.4914, 
+            np.ones((1, 32, 32), dtype=np.float32) * 0.4822, 
+            np.ones((1, 32, 32), dtype=np.float32) * 0.4465], 0)
+
+    return img_noise
+
+def get_cifar10_testset(dir_, batch_size=1):
+    cifar10_mean = (0.4914, 0.4822, 0.4465)
+    cifar10_std = (0.2471, 0.2435, 0.2616)
+    test_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(cifar10_mean, cifar10_std)
+    ])
+    num_workers = 2
+    test_dataset = datasets.CIFAR10(
+        dir_, train=False, transform=test_transform, download=True)
+    test_loader = torch.utils.data.DataLoader(
+        dataset=test_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        pin_memory=True,
+        num_workers=2,
+    )
+    return list(test_loader)
 
 def plot(x, y = None, title = None, xlabel = None, ylabel = None, save = False, path = None):
     """
